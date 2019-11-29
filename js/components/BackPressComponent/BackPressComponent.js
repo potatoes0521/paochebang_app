127354c015a13a6381f4d4d741170b5862e721aa
liuyang <liuy@paoche56.com>
2019-11-29 11:24:56 +0800

    处理安卓返回键

--- js/components/BackPressComponent/BackPressComponent.js
+++ js/components/BackPressComponent/BackPressComponent.js
@@ -1,9 +1,16 @@
+/*
+ * @Author: liuYang
+ * @description: 处理安卓物理返回键
+ * @Date: 2019-11-25 16:29:01
+ * @LastEditors: liuYang
+ * @LastEditTime: 2019-11-29 11:22:55
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ */
+// eslint-disable-next-line no-unused-vars
 import React, {PropTypes} from 'react';
 import {BackHandler} from 'react-native';
 
-/**
- * Android物理回退键处理
- */
 export default class BackPressComponent {
   constructor(props) {
     this._hardwareBackPress = this.onHardwareBackPress.bind(this);
