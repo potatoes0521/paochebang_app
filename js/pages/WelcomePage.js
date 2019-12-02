a8efd58c1581980fe4fae6a9323b201ffb47844a
liuyang <liuy@paoche56.com>
2019-12-02 14:54:05 +0800

    处理开机白屏

--- js/pages/WelcomePage.js
+++ js/pages/WelcomePage.js
@@ -3,13 +3,14 @@
  * @description: 请填写描述信息
  * @Date: 2019-11-22 16:11:20
  * @LastEditors: liuYang
- * @LastEditTime: 2019-11-23 15:36:39
+ * @LastEditTime: 2019-12-02 14:04:12
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import NavigationUtil from '../navigator/NavigationUtils';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class WelcomePage extends Component {
   // constructor(props) {
@@ -18,6 +19,7 @@ export default class WelcomePage extends Component {
 
   componentDidMount() {
     this.timer = setTimeout(() => {
+      SplashScreen.hide();
       // 跳转到首页
       NavigationUtil.resetToHomPage(this.props);
     }, 1000);
