bcdd3ba48a30a46de6e5beea5589d9c5382bea5a
liuyang <liuy@paoche56.com>
2019-12-02 16:48:26 +0800

    自定义iconfon

--- js/pages/Index/components/SellingItem.js
+++ js/pages/Index/components/SellingItem.js
@@ -3,7 +3,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-12-02 14:14:44
  * @LastEditors: liuYang
- * @LastEditTime: 2019-12-02 15:54:53
+ * @LastEditTime: 2019-12-02 16:39:21
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
