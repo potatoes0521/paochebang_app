c9c8881d6978bfedd8c0e413d524eea8df834ffe
liuyang <liuy@paoche56.com>
2019-12-03 15:18:35 +0800

    请求封装

--- js/api/requestHandle.js
+++ js/api/requestHandle.js
@@ -4,10 +4,11 @@
  * @description: 请填写描述信息
  * @Date: 2019-12-02 10:21:17
  * @LastEditors: liuYang
- * @LastEditTime: 2019-12-02 14:12:54
+ * @LastEditTime: 2019-12-03 14:57:50
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
+import axios from 'axios';
 import {defaultApiURL} from '../config/requestConfig.js';
 import createSignData from './secret.js';
 
@@ -15,61 +16,21 @@ const sign_id = 'wxb633da0aa161b42c';
 const contentType = 'application/json;charset=UTF-8';
 export const appVersion = '0.9.2';
 
-export default {
-  /**
-   * 获取网络数据
-   * @param url
-   * @param flag
-   * @returns {Promise}
-   */
-  get(url, data, that, loadingTitle = '加载中...') {
-    return this.fetchData({
-      url,
-      data,
-      that,
-      loadingTitle,
-      method: 'GET',
-    });
-  },
-  post(url, data, that, loadingTitle = '提交中...') {
-    return this.fetchData({
-      url,
-      data,
-      that,
-      loadingTitle,
-      method: 'POST',
-    });
-  },
-  fetchData({url, data, that, method = 'GET', loadingTitle}) {
-    const config = Object.assign(this.baseOptions(url, data, that, method));
-    return new Promise((resolve, reject) => {
-      fetch(config.requestURL, config)
-        .then(response => {
-          console.log(response, 'xxxxxxx');
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error('Network response was not ok.');
-        })
-        .then(responseData => {
-          this.saveData(url, responseData);
-          console.log(responseData, 'xxxxxxxxxxxxxxxxxxxx');
-          resolve(responseData);
-        })
-        .catch(error => {
-          console.log(error);
-          reject(error);
-        });
-    });
-  },
-  baseOptions(url, data, that, method) {
+class HttpRequest {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+  request(options) {
+    const instance = axios.create();
+    options = Object.assign(this.getInsideConfig(options), options);
+    this.interceptors(instance, options.url);
+    return instance(options);
+  }
+  getInsideConfig(options) {
+    let {url, data, that, method} = options;
     const sign = createSignData(data, sign_id)[1];
     // let loadingTimer = null;
     console.log(JSON.stringify(data), 'sign=' + sign, url);
-    let requestURL = defaultApiURL + url;
-    if (url.indexOf('file/read') !== -1) {
-      requestURL = url;
-    }
     const {userInfo} = that.props || {};
     const headerUserLogin = JSON.stringify({
       token: userInfo.token || '',
@@ -79,10 +40,13 @@ export default {
       unionId: userInfo.unionId || '',
       userType: userInfo.userType || '', // 0 驿站人员  1 自主注册   2 驿站人员添加客户 3 运力  4 司机
     });
-
+    if (url.indexOf('file/read') !== -1) {
+      this.baseUrl = url;
+    }
+    console.log(this.baseUrl);
     let config = {
       method,
-      header: {
+      headers: {
         'content-type': contentType,
         'user-login': headerUserLogin,
         sign: sign || '',
@@ -93,32 +57,48 @@ export default {
         'app-type': 1, // 1 微信小程序 2 支付宝小程序  3 PC运营后台
         'system-id': 1, // 1 跑车帮   2 跑车物流  3 运营后台
       },
-      requestURL,
+      baseURL: this.baseUrl,
     };
-    // 如果不是post模式传body会报错
-    if (method === 'POST') {
-      config = Object.assign({}, {body: data}, config);
-    } else {
-      // get方式传参
-      let paramsArray = [];
-      for (var objKey in data) {
-        if (data.hasOwnProperty(objKey)) {
-          var value = data[objKey];
-          var item = {};
-          item.objKey = objKey;
-          item.value = value;
-          paramsArray.push(objKey);
-        }
-      }
-      // 如果请求接口里有?就直接拼接 没有问号就加?拼接到url上
-      if (requestURL.search(/\?/) === -1) {
-        url += '?' + paramsArray.join('&');
-      } else {
-        url += '&' + paramsArray.join('&');
-      }
+    // get方式传参
+    if (method === 'get') {
+      config = Object.assign({}, config, {
+        params: options.data,
+      });
     }
     return config;
-  },
+  }
+  interceptors(instance, url) {
+    // 请求拦截
+    instance.interceptors.request.use(
+      config => {
+        // 添加全局的loading...
+        return config;
+      },
+      error => {
+        return Promise.reject(error);
+      },
+    );
+    // 响应拦截
+    instance.interceptors.response.use(
+      res => {
+        const {data} = res;
+        if (+data.code === 200) {
+          return data;
+        } else if (+data.code === 200003) {
+          console.log('token shixiao');
+          return data;
+        } else {
+          console.error('error 接口错误');
+          return data;
+        }
+      },
+      error => {
+        console.log(error);
+        // addErrorLog(errorInfo);
+        return Promise.reject(error);
+      },
+    );
+  }
   /**
    * 检查timestamp是否在有效期内
    * @param timestamp 项目更新时间
@@ -139,5 +119,33 @@ export default {
     } //有效期4个小时
     // if (currentDate.getMinutes() - targetDate.getMinutes() > 1)return false;
     return true;
+  }
+}
+const newAxios = new HttpRequest(defaultApiURL);
+
+export default {
+  /**
+   * 获取网络数据
+   * @param url
+   * @param flag
+   * @returns {Promise}
+   */
+  get(url, data, that, loadingTitle = '加载中...') {
+    return newAxios.request({
+      url,
+      data,
+      that,
+      loadingTitle,
+      method: 'get',
+    });
+  },
+  post(url, data, that, loadingTitle = '提交中...') {
+    return newAxios.request({
+      url,
+      data,
+      that,
+      loadingTitle,
+      method: 'post',
+    });
   },
 };
