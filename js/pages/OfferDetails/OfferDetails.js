1e3831c74f91563e0382afba8f2c2feb7e654399
liuyang <liuy@paoche56.com>
2019-12-23 10:59:23 +0800

    优化车辆信息样式

--- js/pages/OfferDetails/OfferDetails.js
+++ js/pages/OfferDetails/OfferDetails.js
@@ -4,7 +4,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-12-03 16:47:37
  * @LastEditors  : liuYang
- * @LastEditTime : 2019-12-20 16:34:50
+ * @LastEditTime : 2019-12-23 10:57:36
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -363,7 +363,11 @@ class OfferDetails extends Component {
                 </View>
               ) : null}
               {/* 车辆信息 */}
-              <View style={DetailsStyles.formItem}>
+              <View
+                style={[
+                  DetailsStyles.formItem,
+                  DetailsStyles.moreTextFormItem,
+                ]}>
                 <View style={DetailsStyles.formLabel}>
                   <Text style={DetailsStyles.labelText}>车辆信息:</Text>
                 </View>
