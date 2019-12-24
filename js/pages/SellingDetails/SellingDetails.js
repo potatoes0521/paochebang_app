1da04a6a697583044e62d89b15343fd4a5007f11
liuyang <liuy@paoche56.com>
2019-12-24 17:00:49 +0800

    卖板和空位详情

--- js/pages/SellingDetails/SellingDetails.js
+++ js/pages/SellingDetails/SellingDetails.js
@@ -3,30 +3,57 @@
  * @description: 请填写描述信息
  * @path: 引入路径
  * @Date: 2019-12-24 11:45:16
- * @LastEditors: liuYang
- * @LastEditTime: 2019-12-24 11:48:00
+ * @LastEditors  : liuYang
+ * @LastEditTime : 2019-12-24 17:00:15
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, Linking} from 'react-native';
 import {connect} from 'react-redux';
 import GlobalStyles from '../../assets/css/GlobalStyles';
+import DetailsStyles from '../../assets/css/DetailsStyles';
 import NavigationUtil from '../../navigator/NavigationUtils';
 import BackPressComponent from '../../components/BackPressComponent/BackPressComponent';
 import NavigationBar from '../../components/NavigatorBar/NavigationBar';
 import SafeAreaViewPlus from '../../components/SafeAreaViewPlus/SafeAreaViewPlus';
+import {payWey} from '../../config/text_config.js';
+import api from '../../api/index.js';
+import Button from '../../components/Button/Button.js';
+import Toast from 'react-native-easy-toast';
 
 class SellingDetails extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      carAmount: '', // 空位数
+      carInfo: '', // 车辆信息
+      dueTime: '', // 有效期
+      isActive: 1, // 订单状态
+      payType: '', // 支付方式
+      returnPrice: '',
+      pubTime: '', // 发布时间
+      receiveCityName: '', // 收车城市
+      remarks: '', // 备注
+      sendCityName: '', // 发车城市
+      sendTime: '', // 发车时间
+      saleToPalletId: '', // 卖板信息ID
+      isEdit: '', // 是否可编辑
+      usedType: '', // 车辆性质
+    };
     this.backPress = new BackPressComponent({
       backPress: () => this.onBackPress(),
     });
+    this.pageParams = {};
   }
 
   componentDidMount() {
+    const {navigation} = this.props;
+    const {state} = navigation;
+    const {params} = state;
+    console.log('params', params);
+    this.pageParams = params || {};
+    this.getSellingDetail();
     this.backPress.componentDidMount();
   }
 
@@ -39,12 +66,258 @@ class SellingDetails extends Component {
     return true;
   }
 
+  /**
+   * 获取详情
+   * @return void
+   */
+  getSellingDetail() {
+    if (!this.pageParams.saleToPalletId) {
+      this.refs.toast.show('缺少saleToPalletId或saleToPalletCode');
+      return;
+    }
+    let sendData = {
+      id: this.pageParams.saleToPalletId,
+    };
+    api.selling.getSellingDetail(sendData, this).then(res => {
+      if (!res.data) {
+        return;
+      }
+      console.log('res.data', res.data);
+      this.setState(res.data);
+    });
+  }
+  /**
+   * 立即联系
+   * @return void
+   */
+  callHim() {
+    const tel = `tel:${this.state.mobile}`;
+    if (this.state.isActive !== 1) {
+      return;
+    }
+    Linking.canOpenURL(tel)
+      .then(supported => {
+        if (!supported) {
+          console.log('Can not handle tel:' + tel);
+        } else {
+          return Linking.openURL(tel);
+        }
+      })
+      .catch(error => console.log('tel error', error));
+  }
+  /**
+   * 下架
+   * @return void
+   */
+  pullOff() {
+    if (this.state.isActive !== 1) {
+      return;
+    }
+    let sendData = {
+      saleToPalletId: this.state.saleToPalletId,
+    };
+    api.selling.sellingDataPullOff(sendData, this).then(() => {
+      this.refs.toast.show('下架成功');
+      setTimeout(() => {
+        NavigationUtil.goBack(this.props.navigation);
+      }, 1800);
+    });
+  }
+  navigatorEdit() {
+    NavigationUtil.goPage(this.pageParams, 'EditSellingPage');
+  }
+
   render() {
-    const {theme} = this.props;
+    const {theme, navigation} = this.props;
+    let {
+      carAmount,
+      carInfo,
+      dueTime,
+      isActive,
+      payType,
+      returnPrice,
+      pubTime,
+      receiveCityName,
+      remarks,
+      sendCityName,
+      sendTime,
+      usedType,
+      isEdit,
+    } = this.state;
+    const payWeyText = payWey.filter(item => {
+      return +item.id === +payType;
+    })[0];
+    let textClassName = [DetailsStyles.contentText];
+    if (isActive !== 1) {
+      textClassName.push(DetailsStyles.textThemeDisabled);
+    }
     return (
       <SafeAreaViewPlus topColor={theme.themeColor}>
         <View style={styles.pageWrapper}>
-          <Text>SellingDetails</Text>
+          <NavigationBar
+            navigation={navigation}
+            leftViewShow={true}
+            title={'卖板详情'}
+          />
+          <ScrollView>
+            <View style={DetailsStyles.card}>
+              {/* 发车城市 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>发车城市:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {sendCityName || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 收车城市 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>收车城市:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {receiveCityName || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 预计发车时间 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>预计发车时间:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {sendTime.split('T')[0] || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 车辆信息 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>车辆信息:</Text>
+                </View>
+                <View
+                  style={[
+                    DetailsStyles.formContent,
+                    DetailsStyles.moreTextFormItem,
+                  ]}>
+                  <Text style={DetailsStyles.contentText}>{carInfo || ''}</Text>
+                </View>
+              </View>
+              {/* 车辆性质 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>车辆性质:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {usedType === 1 ? '新车' : '二手车'}
+                  </Text>
+                </View>
+              </View>
+              {/* 台数 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>台数:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {carAmount || '0'}
+                  </Text>
+                </View>
+              </View>
+              {/* 结算方式 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>结算方式:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {payWeyText ? payWeyText.name : ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 报价 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>报价:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {returnPrice || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 有效期至 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>有效期至:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {dueTime.split('T')[0] || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 发布时间 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>发布时间:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {pubTime.split('T')[0] || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 备注 */}
+              {remarks ? (
+                <View style={DetailsStyles.formItem}>
+                  <View style={DetailsStyles.formLabel}>
+                    <Text style={DetailsStyles.labelText}>备注:</Text>
+                  </View>
+                  <View
+                    style={[
+                      DetailsStyles.formContent,
+                      DetailsStyles.moreTextFormItem,
+                    ]}>
+                    <Text style={DetailsStyles.contentText}>
+                      {remarks || ''}
+                    </Text>
+                  </View>
+                </View>
+              ) : null}
+            </View>
+            <View style={DetailsStyles.btnWrapper}>
+              {isEdit !== '' && isEdit ? (
+                <>
+                  <Button
+                    type={'plain'}
+                    btnStyle={[DetailsStyles.btnLeft]}
+                    text={'编辑'}
+                    onClick={this.navigatorEdit.bind(this)}
+                  />
+                  <Button
+                    btnStyle={[DetailsStyles.btnRight]}
+                    text={'下架'}
+                    type={'round'}
+                    onClick={this.pullOffer.bind(this)}
+                  />
+                </>
+              ) : (
+                <Button
+                  btnStyle={[DetailsStyles.btnRight]}
+                  text={'立即联系'}
+                  type={'round'}
+                  onClick={this.callHim.bind(this)}
+                />
+              )}
+            </View>
+          </ScrollView>
+          <Toast ref="toast" position={'center'} defaultCloseDelay={3000} />
         </View>
       </SafeAreaViewPlus>
     );
@@ -54,6 +327,7 @@ class SellingDetails extends Component {
 const styles = StyleSheet.create({
   pageWrapper: {
     flex: 1,
+    backgroundColor: '#f5f5f5',
   },
 });
 // 如果需要引入store
--- js/pages/VacancyDetails/VacancyDetails.js
+++ js/pages/VacancyDetails/VacancyDetails.js
@@ -3,30 +3,55 @@
  * @description: 请填写描述信息
  * @path: 引入路径
  * @Date: 2019-12-24 11:48:37
- * @LastEditors: liuYang
- * @LastEditTime: 2019-12-24 11:50:18
+ * @LastEditors  : liuYang
+ * @LastEditTime : 2019-12-24 17:00:01
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, Linking} from 'react-native';
 import {connect} from 'react-redux';
 import GlobalStyles from '../../assets/css/GlobalStyles';
+import DetailsStyles from '../../assets/css/DetailsStyles';
 import NavigationUtil from '../../navigator/NavigationUtils';
 import BackPressComponent from '../../components/BackPressComponent/BackPressComponent';
 import NavigationBar from '../../components/NavigatorBar/NavigationBar';
 import SafeAreaViewPlus from '../../components/SafeAreaViewPlus/SafeAreaViewPlus';
+import api from '../../api/index.js';
+import Button from '../../components/Button/Button.js';
+import Toast from 'react-native-easy-toast';
 
 class VacancyDetsils extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      vacantAmount: '', // 空位数
+      dueTime: '', // 有效期
+      isActive: 1, // 订单状态
+      price: '', // 价格
+      returnPrice: '',
+      pubTime: '', // 发布时间
+      receiveCityName: '', // 收车城市
+      remarks: '', // 备注
+      sendCityName: '', // 发车城市
+      throughCitys: '', // 途经城市
+      startTime: '', // 发车时间
+      vacantPalletId: '', // 空位信息ID
+      isEdit: '', // 是否可编辑
+      usedType: '', // 车辆性质
+    };
     this.backPress = new BackPressComponent({
       backPress: () => this.onBackPress(),
     });
   }
 
   componentDidMount() {
+    const {navigation} = this.props;
+    const {state} = navigation;
+    const {params} = state;
+    console.log('params', params);
+    this.pageParams = params || {};
+    this.getVacancyDetail();
     this.backPress.componentDidMount();
   }
 
@@ -38,13 +63,221 @@ class VacancyDetsils extends Component {
     NavigationUtil.goBack(this.props.navigation);
     return true;
   }
-
+  /**
+   * 获取空位详情
+   * @return void
+   */
+  getVacancyDetail() {
+    if (!this.pageParams.vacantPalletId) {
+      this.refs.toast.show('缺少vacantPalletId或vacantPalletCode');
+      return;
+    }
+    let sendData = {
+      id: this.pageParams.vacantPalletId,
+    };
+    api.vacancy.getVacancyDetail(sendData, this).then(res => {
+      if (!res.data) {
+        return;
+      }
+      let data = Object.assign({}, res.data, {
+        throughCitys: {
+          cityName: res.throughCitiesName || '',
+          cityId: res.throughCitys,
+        },
+      });
+      this.setState(data, () => {
+        console.log('this.state', this.state);
+      });
+    });
+  }
+  /**
+   * 立即联系
+   * @return void
+   */
+  callHim() {
+    const tel = `tel:${this.state.mobile}`;
+    if (this.state.isActive !== 1) {
+      return;
+    }
+    Linking.canOpenURL(tel)
+      .then(supported => {
+        if (!supported) {
+          console.log('Can not handle tel:' + tel);
+        } else {
+          return Linking.openURL(tel);
+        }
+      })
+      .catch(error => console.log('tel error', error));
+  }
+  /**
+   * 下架
+   * @return void
+   */
+  pullOff() {
+    if (this.state.isActive !== 1) {
+      return;
+    }
+    let sendData = {
+      saleToPalletId: this.state.saleToPalletId,
+    };
+    api.vacancy.sellingDataPullOff(sendData, this).then(() => {
+      this.refs.toast.show('下架成功');
+      setTimeout(() => {
+        NavigationUtil.goBack(this.props.navigation);
+      }, 1800);
+    });
+  }
+  navigatorEdit() {
+    NavigationUtil.goPage(this.pageParams, 'EditVacancyPage');
+  }
   render() {
-    const {theme} = this.props;
+    const {theme, navigation} = this.props;
+    let {
+      vacantAmount,
+      dueTime,
+      isActive,
+      returnPrice,
+      throughCitys,
+      receiveCityName,
+      remarks,
+      sendCityName,
+      startTime,
+      isEdit,
+    } = this.state;
     return (
       <SafeAreaViewPlus topColor={theme.themeColor}>
         <View style={styles.pageWrapper}>
-          <Text>VacancyDetsils</Text>
+          <NavigationBar
+            navigation={navigation}
+            leftViewShow={true}
+            title={'空位详情'}
+          />
+          <ScrollView>
+            <View style={DetailsStyles.card}>
+              {/* 发车城市 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>发车城市:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {sendCityName || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 收车城市 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>收车城市:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {receiveCityName || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 途经城市 */}
+              {throughCitys && throughCitys.cityName ? (
+                <View style={DetailsStyles.formItem}>
+                  <View style={DetailsStyles.formLabel}>
+                    <Text style={DetailsStyles.labelText}>途经城市:</Text>
+                  </View>
+                  <View style={DetailsStyles.formContent}>
+                    <Text style={DetailsStyles.contentText}>
+                      {throughCitys.cityName || ''}
+                    </Text>
+                  </View>
+                </View>
+              ) : null}
+              {/* 出发时间 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>出发时间:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {startTime.split('T')[0] || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 余位 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>余位:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {vacantAmount || '0'}
+                  </Text>
+                </View>
+              </View>
+              {/* 有效期至 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>有效期至:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {dueTime.split('T')[0] || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 报价 */}
+              <View style={DetailsStyles.formItem}>
+                <View style={DetailsStyles.formLabel}>
+                  <Text style={DetailsStyles.labelText}>报价:</Text>
+                </View>
+                <View style={DetailsStyles.formContent}>
+                  <Text style={DetailsStyles.contentText}>
+                    {returnPrice || ''}
+                  </Text>
+                </View>
+              </View>
+              {/* 备注 */}
+              {remarks ? (
+                <View style={DetailsStyles.formItem}>
+                  <View style={DetailsStyles.formLabel}>
+                    <Text style={DetailsStyles.labelText}>备注:</Text>
+                  </View>
+                  <View
+                    style={[
+                      DetailsStyles.formContent,
+                      DetailsStyles.moreTextFormItem,
+                    ]}>
+                    <Text style={DetailsStyles.contentText}>
+                      {remarks || ''}
+                    </Text>
+                  </View>
+                </View>
+              ) : null}
+            </View>
+            <View style={DetailsStyles.btnWrapper}>
+              {isEdit !== '' && isEdit ? (
+                <>
+                  <Button
+                    type={'plain'}
+                    btnStyle={[DetailsStyles.btnLeft]}
+                    text={'编辑'}
+                    onClick={this.navigatorEdit.bind(this)}
+                  />
+                  <Button
+                    btnStyle={[DetailsStyles.btnRight]}
+                    text={'下架'}
+                    type={'round'}
+                    onClick={this.pullOffer.bind(this)}
+                  />
+                </>
+              ) : (
+                <Button
+                  btnStyle={[DetailsStyles.btnRight]}
+                  text={'立即联系'}
+                  type={'round'}
+                  onClick={this.callHim.bind(this)}
+                />
+              )}
+            </View>
+          </ScrollView>
+          <Toast ref="toast" position={'center'} defaultCloseDelay={3000} />
         </View>
       </SafeAreaViewPlus>
     );
@@ -54,6 +287,7 @@ class VacancyDetsils extends Component {
 const styles = StyleSheet.create({
   pageWrapper: {
     flex: 1,
+    backgroundColor: '#f5f5f5',
   },
 });
 // 如果需要引入store
