36731e367a4f50fee909d848d07ab645151accc1
liuyang <liuy@paoche56.com>
2019-12-24 11:37:29 +0800

    欢迎页的修改

--- js/pages/WelcomePage.js
+++ js/pages/WelcomePage.js
@@ -2,8 +2,8 @@
  * @Author: liuYang
  * @description: 请填写描述信息
  * @Date: 2019-11-22 16:11:20
- * @LastEditors: liuYang
- * @LastEditTime: 2019-12-02 14:04:12
+ * @LastEditors  : liuYang
+ * @LastEditTime : 2019-12-24 11:36:01
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -11,8 +11,10 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import NavigationUtil from '../navigator/NavigationUtils';
 import SplashScreen from 'react-native-splash-screen';
+import {connect} from 'react-redux';
+import SafeAreaViewPlus from '../components/SafeAreaViewPlus/SafeAreaViewPlus';
 
-export default class WelcomePage extends Component {
+class WelcomePage extends Component {
   // constructor(props) {
   //   super(props);
   // }
@@ -30,15 +32,26 @@ export default class WelcomePage extends Component {
   }
 
   render() {
+    let {theme} = this.props;
+    console.log('theme', theme);
     return (
-      <View style={style.pageWrapper}>
-        <Text>欢迎页</Text>
-      </View>
+      <SafeAreaViewPlus topColor={theme.themeColor}>
+        <View style={styles.pageWrapper}>
+          <Text>欢迎页</Text>
+        </View>
+      </SafeAreaViewPlus>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    userInfo: state.user_info.userInfo,
+    theme: state.theme.theme,
+  };
+};
+export default connect(mapStateToProps)(WelcomePage);
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   pageWrapper: {
     flex: 1,
     justifyContent: 'center',
