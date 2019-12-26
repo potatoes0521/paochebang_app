fbf601102978c83ab052ac0777eb31dbfcf9070c
liuyang <liuy@paoche56.com>
2019-12-26 14:56:29 +0800

    添加启动白屏处理

--- android/app/src/main/java/com/paochebang_app/MainActivity.java
+++ android/app/src/main/java/com/paochebang_app/MainActivity.java
@@ -2,20 +2,26 @@
  * @Author: liuYang
  * @description: 请填写描述信息
  * @Date: 2019-11-22 13:51:52
- * @LastEditors: liuYang
- * @LastEditTime: 2019-11-22 16:39:51
+ * @LastEditors  : liuYang
+ * @LastEditTime : 2019-12-26 14:42:10
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 package com.paochebang_app;
 
+import android.os.Bundle; // here
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.facebook.react.ReactActivity;
+
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
-
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -23,6 +29,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "paochebang_app";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); // here
+    super.onCreate(savedInstanceState);
   }
 
   /**
