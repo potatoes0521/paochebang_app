0365d42b05ba3cd41cdb7c5b330bdeea02bdf7e3
guor <guor@paoche56.com>
2019-12-27 17:04:10 +0800

    解决冲突

--- js/navigator/AppNavigators.js
+++ js/navigator/AppNavigators.js
@@ -3,7 +3,7 @@
  * @description: 页面/navigator配置
  * @Date: 2019-11-22 16:52:09
  * @LastEditors  : guorui
- * @LastEditTime : 2019-12-27 17:02:41
+ * @LastEditTime : 2019-12-27 17:03:32
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
