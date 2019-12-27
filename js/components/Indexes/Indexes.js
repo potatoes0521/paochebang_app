73829b7ac4b0c130193047078e22c3a70ace8646
liuyang <liuy@paoche56.com>
2019-12-27 09:10:50 +0800

    选择城市

--- js/components/Indexes/Indexes.js
+++ js/components/Indexes/Indexes.js
@@ -4,42 +4,101 @@
  * @path: 引入路径
  * @Date: 2019-12-26 09:42:31
  * @LastEditors  : liuYang
- * @LastEditTime : 2019-12-26 11:59:34
+ * @LastEditTime : 2019-12-26 18:09:08
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, SectionList} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  TouchableOpacity,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import GlobalStyles from '../../assets/css/GlobalStyles';
+const ITEM_HEIGHT = 50; //item的高度
+const HEADER_HEIGHT = 24; //分组头部的高度
+const SEPARATOR_HEIGHT = 0; //分割线的高度
 
 export default class Indexes extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.indexes = React.createRef();
   }
 
   componentDidMount() {}
 
   componentWillUnmount() {}
-
+  _getItemLayout(data, index) {
+    let [length, separator, header] = [
+      ITEM_HEIGHT,
+      SEPARATOR_HEIGHT,
+      HEADER_HEIGHT,
+    ];
+    return {
+      length,
+      offset: (length + separator) * index + header,
+      index,
+    };
+  }
+  jumpTarget(index) {
+    console.log('this.indexes.current', this.indexes.current);
+    this.indexes.current.scrollToLocation({
+      animated: true,
+      sectionIndex: Math.round(index),
+      itemIndex: 0,
+      viewOffset: 75,
+    });
+  }
   render() {
     console.log('this.props.data', this.props.data);
     let {data} = this.props;
+    const keyList = data.map((item, index) => (
+      <TouchableOpacity
+        onPress={this.jumpTarget.bind(this, index)}
+        style={styles.listItem}>
+        <Text style={styles.listItemText}>{item.initial}</Text>
+      </TouchableOpacity>
+    ));
     return (
       <View style={styles.pageWrapper}>
         <SectionList
+          ref={this.indexes}
           sections={data}
-          renderItem={({item}, index) => {
-            console.log('item, index', item);
-            return <Text key={index}>{item.cityName}</Text>;
+          initialNumToRender={50}
+          renderItem={({item, index, section}) => {
+            return (
+              <>
+                <View style={styles.sectionItem}>
+                  <Text style={styles.sectionItemText} key={index}>
+                    {item.cityName}
+                  </Text>
+                </View>
+                {index !== section.data.length - 1 && (
+                  <View style={styles.line} />
+                )}
+              </>
+            );
           }}
           renderSectionHeader={({section}) => {
-            console.log('sections', section);
-            return <Text>{section.initial}</Text>;
+            return (
+              <View style={styles.sectionTitle}>
+                <Text style={styles.sectionTitleText}>{section.initial}</Text>
+              </View>
+            );
           }}
-          keyExtractor={(item, index) => index}
+          keyExtractor={(item, index) => {
+            return item.cityId;
+          }}
+          getItemLayout={this._getItemLayout}
+          onScrollToIndexFailed={() => {
+            console.log('object');
+          }}
         />
+        <View style={styles.keyList}>{keyList}</View>
       </View>
     );
   }
@@ -48,6 +107,45 @@ export default class Indexes extends Component {
 const styles = StyleSheet.create({
   pageWrapper: {
     flex: 1,
+  },
+  sectionTitle: {
+    flexDirection: 'row',
+    height: 25,
+    backgroundColor: '#f7f7f7',
+    alignItems: 'center',
+    paddingLeft: 12,
+  },
+  sectionTitleText: {
+    color: GlobalStyles.themeHColor,
+    fontSize: 16,
+  },
+  sectionItem: {
+    marginLeft: 12,
+    marginRight: 30,
+    paddingVertical: 13,
+  },
+  line: {
+    height: 1,
+    backgroundColor: '#f5f5f5',
+    marginLeft: 12,
+    marginRight: 30,
+  },
+  keyList: {
+    position: 'absolute',
+    right: 12,
+    height: GlobalStyles.window_height,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    height: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 2,
+  },
+  listItemText: {
+    color: GlobalStyles.themeColor,
   },
 });
 
--- js/pages/ChooseCity/ChooseCity.js
+++ js/pages/ChooseCity/ChooseCity.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2019-12-26 09:24:29
  * @LastEditors  : liuYang
- * @LastEditTime : 2019-12-26 12:00:23
+ * @LastEditTime : 2019-12-26 18:59:59
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -19,6 +19,7 @@ import SafeAreaViewPlus from '../../components/SafeAreaViewPlus/SafeAreaViewPlus
 import Indexes from '../../components/Indexes/Indexes';
 import _flattenDeep from 'lodash/flattenDeep';
 import api from '../../api/index';
+import {TextInput} from 'react-native-gesture-handler';
 
 class ChooseCity extends Component {
   constructor(props) {
@@ -36,6 +37,7 @@ class ChooseCity extends Component {
     this.throughCityNameList = [];
     this.throughCityIdList = [];
     this.pageParams = {};
+    this.searchValue = '';
   }
 
   componentDidMount() {
@@ -86,6 +88,10 @@ class ChooseCity extends Component {
   chooseCity(city) {
     console.log('city', city);
   }
+  searchInput(value) {
+    console.log('value', value);
+    this.searchValue = value;
+  }
   render() {
     const {theme, navigation} = this.props;
     const {allCity} = this.state;
@@ -97,7 +103,20 @@ class ChooseCity extends Component {
             leftViewShow={true}
             title={'选择城市'}
           />
-          <Indexes data={allCity} onClick={this.chooseCity.bind(this)} />
+          <View style={styles.searchWrapper}>
+            <View style={styles.searchIconWrapper}>
+              <Text style={styles.searchIcon}>&#xe604;</Text>
+            </View>
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.input}
+                placeholder={'请输入城市名称进行搜索'}
+                onChangeText={this.searchInput.bind(this)}
+              />
+            </View>
+          </View>
+          <View style={styles.wrapperLine} />
+          {/* <Indexes data={allCity} onClick={this.chooseCity.bind(this)} /> */}
         </View>
       </SafeAreaViewPlus>
     );
@@ -107,6 +126,40 @@ class ChooseCity extends Component {
 const styles = StyleSheet.create({
   pageWrapper: {
     flex: 1,
+  },
+  searchWrapper: {
+    marginVertical: 8,
+    marginHorizontal: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    height: 38,
+    borderRadius: 4,
+  },
+  searchIconWrapper: {
+    width: 32,
+    height: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchIcon: {
+    fontSize: 20,
+    color: GlobalStyles.themeHColor,
+    fontFamily: 'iconfont',
+  },
+  inputWrapper: {
+    flex: 1,
+  },
+  input: {
+    padding: 0,
+    margin: 0,
+    color: GlobalStyles.themeFontColor,
+    fontSize: 15,
+  },
+  wrapperLine: {
+    height: 8,
+    backgroundColor: '#f5f5f5',
   },
 });
 // 如果需要引入store
