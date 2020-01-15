edb6da5d0c3eca28a3ee6ab1c21829c9450dfbbf
liuyang <liuy@paoche56.com>
2020-01-15 16:17:33 +0800

    注释本地的userInfo

--- js/store/reducer/user/index.js
+++ js/store/reducer/user/index.js
@@ -3,7 +3,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-08-15 15:26:09
  * @LastEditors  : liuYang
- * @LastEditTime : 2020-01-10 14:02:20
+ * @LastEditTime : 2020-01-15 15:17:20
  */
 
 import Types from '../../type/index';
@@ -18,29 +18,29 @@ const defaultState = {
 
 // if (env === 'development') {
 defaultState.userInfo = {
-  appVersion: null,
-  createTime: '2019-11-13T06:08:32.000+0000',
-  dueTime: '2019-12-05T04:00:09.000+0000',
-  isActive: 1,
-  loginId: 479,
-  loginType: 2,
-  systemId: 1,
-  terminalIp: '192.168.3.191',
-  realNameAuthStatus: null,
-  terminalType: 1,
-  mobile: '13370130024',
-  // mobile: "13312345678",
-  // openId: "11akshahsdgjhasgk", // 测试环境被添加的客户的openid   手机号是  13312345678
-  // userId: 177,
-  // userType: 2,
-  userType: 1, // 13121850481
-  openId: 'oLgd75eZdM9Cp-dSE0YpU9gKxJoE', // 测试环境刘洋的openId
-  // userId: 93, // 正式刘洋的userId
-  // token: '006c043c79f7555d1c327ff64ade066c', // 正式token
-  userId: 250, // 测试刘洋的userId
-  token: '1d30ce249869f6efce7b0a8303064c21', // 测试token
-  updateTime: '2019-11-28T04:00:09.000+0000',
-  userAgent: '',
+  // appVersion: null,
+  // createTime: '2019-11-13T06:08:32.000+0000',
+  // dueTime: '2019-12-05T04:00:09.000+0000',
+  // isActive: 1,
+  // loginId: 479,
+  // loginType: 2,
+  // systemId: 1,
+  // terminalIp: '192.168.3.191',
+  // realNameAuthStatus: null,
+  // terminalType: 1,
+  // mobile: '13370130024',
+  // // mobile: "13312345678",
+  // // openId: "11akshahsdgjhasgk", // 测试环境被添加的客户的openid   手机号是  13312345678
+  // // userId: 177,
+  // // userType: 2,
+  // userType: 1, // 13121850481
+  // openId: 'oLgd75eZdM9Cp-dSE0YpU9gKxJoE', // 测试环境刘洋的openId
+  // // userId: 93, // 正式刘洋的userId
+  // // token: '006c043c79f7555d1c327ff64ade066c', // 正式token
+  // userId: 250, // 测试刘洋的userId
+  // token: '1d30ce249869f6efce7b0a8303064c21', // 测试token
+  // updateTime: '2019-11-28T04:00:09.000+0000',
+  // userAgent: '',
 };
 // }
 
