23ded4604ab5cb53c06e37b2561b7fe04f9a05fa
liuyang <liuy@paoche56.com>
2020-01-15 16:17:53 +0800

    更新请求头信息

--- js/api/requestHandle.js
+++ js/api/requestHandle.js
@@ -4,7 +4,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-12-02 10:21:17
  * @LastEditors  : liuYang
- * @LastEditTime : 2020-01-13 18:18:41
+ * @LastEditTime : 2020-01-15 15:55:49
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -55,14 +55,6 @@ class HttpRequest {
       openId: userInfo.openId || '',
       userId: userInfo.userId || '', // 常用请求全部放在请求头上
       unionId: userInfo.unionId || '',
-      userType: userInfo.userType || '', // 0 驿站人员  1 自主注册   2 驿站人员添加客户 3 运力  4 司机
-    });
-    console.log('headerUserLogin', {
-      token: userInfo.token,
-      mobile: userInfo.mobile,
-      openId: userInfo.openId,
-      userId: userInfo.userId, // 常用请求全部放在请求头上
-      unionId: userInfo.unionId,
       userType: userInfo.userType, // 0 驿站人员  1 自主注册   2 驿站人员添加客户 3 运力  4 司机
     });
     let contentType = 'application/json;charset=UTF-8';
@@ -79,14 +71,14 @@ class HttpRequest {
         'content-type': contentType,
         'user-login': headerUserLogin,
         sign: sign || '',
-        // 'terminal-type': 3, // 终端类型  1 小程序   2 H5  3 APP 4 运营后台
-        'terminal-type': 1, // 终端类型  1 小程序   2 H5  3 APP 4 运营后台
-        // 'source-id': 2, // 1 跑车帮小程序 2 跑车帮app 3 跑车物流小程序 4 跑车物流运营平台
-        'source-id': 1, // 1 跑车帮小程序 2 跑车帮app 3 跑车物流小程序 4 跑车物流运营平台
+        'terminal-type': 3, // 终端类型  1 小程序   2 H5  3 APP 4 运营后台
+        // 'terminal-type': 1, // 终端类型  1 小程序   2 H5  3 APP 4 运营后台
+        'source-id': 2, // 1 跑车帮小程序 2 跑车帮app 3 跑车物流小程序 4 跑车物流运营平台
+        // 'source-id': 1, // 1 跑车帮小程序 2 跑车帮app 3 跑车物流小程序 4 跑车物流运营平台
         'system-info': userInfo.userAgent || 'iPhone 6/7/8-iOS 10.0.1-2.9.4', // 系统信息
         'app-version': appVersion, // 版本号
-        // 'app-type': 4, // 1 微信小程序 2 支付宝小程序  3 PC运营后台
-        'app-type': 1, // 1 微信小程序 2 支付宝小程序  3 PC运营后台
+        'app-type': 4, // 1 微信小程序 2 支付宝小程序  3 PC运营后台
+        // 'app-type': 1, // 1 微信小程序 2 支付宝小程序  3 PC运营后台
         'system-id': 1, // 1 跑车帮   2 跑车物流  3 运营后台
       },
       baseURL: baseUrl,
