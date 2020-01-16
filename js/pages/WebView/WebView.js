d2e223956764d75ecef125afb02fe0e4d140c1a1
liuyang <liuy@paoche56.com>
2020-01-16 14:46:47 +0800

    优化

--- js/pages/WebView/WebView.js
+++ js/pages/WebView/WebView.js
@@ -4,12 +4,12 @@
  * @path: 引入路径
  * @Date: 2020-01-16 14:11:35
  * @LastEditors  : liuYang
- * @LastEditTime : 2020-01-16 14:18:44
+ * @LastEditTime : 2020-01-16 14:46:22
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {connect} from 'react-redux';
 import NavigationUtil from '../../navigator/NavigationUtils';
 import BackPressComponent from '../../components/BackPressComponent/BackPressComponent';
@@ -22,6 +22,7 @@ class WebViewPage extends Component {
     super(props);
     this.state = {
       url: '',
+      canGoBack: false,
     };
     this.backPress = new BackPressComponent({
       backPress: () => this.onBackPress(),
@@ -42,12 +43,25 @@ class WebViewPage extends Component {
   }
 
   onBackPress() {
-    NavigationUtil.goBack(this.props.navigation);
+    console.log('this.state.canGoBack', this.state.canGoBack);
+    console.log('this.state.url', this.state.url);
+    if (this.state.canGoBack) {
+      this.webView.goBack();
+    } else {
+      NavigationUtil.goBack(this.props.navigation);
+    }
     return true;
   }
   handleData(params) {
     this.setState({
       url: decodeURIComponent(params.locationUrl),
+    });
+  }
+  onNavigationStateChange(navState) {
+    console.log('navState', navState);
+    this.setState({
+      canGoBack: navState.canGoBack,
+      url: navState.url,
     });
   }
   render() {
@@ -60,13 +74,15 @@ class WebViewPage extends Component {
             leftViewShow={true}
             title={'跑车帮'}
           />
-          <WebView
-            // style={{marginTop: NAVIGATION_BAR_HEIGHT}}
-            ref={webView => (this.webView = webView)}
-            startInLoadingState={true}
-            // onNavigationStateChange={e => this.onNavigationStateChange(e)}
-            source={{uri: this.state.url}}
-          />
+          {this.state.url ? (
+            <WebView
+              // style={{marginTop: NAVIGATION_BAR_HEIGHT}}
+              ref={webView => (this.webView = webView)}
+              startInLoadingState={true}
+              onNavigationStateChange={e => this.onNavigationStateChange(e)}
+              source={{uri: this.state.url}}
+            />
+          ) : null}
         </View>
       </SafeAreaViewPlus>
     );
