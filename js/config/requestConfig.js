22ff600f3f43d331ba3add7f2518124afa15ff0d
liuyang <liuy@paoche56.com>
2020-01-20 09:20:23 +0800

    修改url

--- js/config/requestConfig.js
+++ js/config/requestConfig.js
@@ -3,12 +3,12 @@
  * @description: 服务器状态码错误配置
  * @Date: 2019-08-13 12:22:58
  * @LastEditors  : liuYang
- * @LastEditTime : 2020-01-19 15:39:04
+ * @LastEditTime : 2020-01-20 09:20:14
  */
 // api地址
-export const defaultApiURL = 'http://api.bang.paoche56.in/'; // 测试环境
+// export const defaultApiURL = 'http://api.bang.paoche56.in/'; // 测试环境
 // export const defaultApiURL = 'http://192.168.3.142:8081/'; // 测试环境
-// export const defaultApiURL = 'https://api.bang.paoche56.com/'; // 正式环境
+export const defaultApiURL = 'https://api.bang.paoche56.com/'; // 正式环境
 // export const defaultApiURL = 'http://api-doc.paoche56.in/mock/19/' // mock
 
 // 上传文件地址
